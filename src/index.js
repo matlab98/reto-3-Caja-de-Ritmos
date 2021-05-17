f68f392019a1deb09503f67dcdf4e2f8598d7ff2
Hilder Arrieta <hbarrieta25@gmail.com>
2021-05-17 13:47:59 -0500

    Configuration initial project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+
 ReactDOM.render(
-    <App/>,
-    document.getElementById('app'))
+    <App />,
+    document.getElementById('app')
+  );
