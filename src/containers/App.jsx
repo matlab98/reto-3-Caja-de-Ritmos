e4c458eb5e9499ea7ec861742203d028cf5628d3
Hilder Arrieta <hbarrieta25@gmail.com>
2021-05-24 18:08:14 -0500

    Lasts commit

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import axios from 'axios';
-import style from '../utils/format.css'
+import style from '../utils/format.css';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+
 export default class App extends React.Component {
-    state = {
-        sounds: []
+    constructor(props) {
+        super();
+        this.state = {
+            sounds: [],
+            activeTab: props.activeTab || 1
+        };
+
     }
 
-   
     componentDidMount() {
         axios.get(`https://reto2-ag.herokuapp.com/Dock`)
             .then(res => {
                 const sounds = res.data;
                 this.setState({ sounds });
             })
-            document.addEventListener("keydown", this.handleKeyDown);
+        document.addEventListener("keydown", this.handleKeyDown);
     }
-    
+
     handleKeyDown = (e) => {
         let key = document.getElementById((`${e.key}`).toUpperCase())
-        if(key){ 
-          key.firstElementChild.play()
-          let display = document.getElementById("display");
-          display.textContent = key.id
-        }  
+        if (key) {
+            key.firstElementChild.play()
+            let display = document.getElementById("display");
+            display.textContent = key.id
+        }
     }
+
 
     render() {
         const handleClick = (e) => {
@@ -32,10 +39,22 @@ export default class App extends React.Component {
             display.textContent = e.target.id
         }
 
+        const changedOne = (e) => {
+            let bank = document.getElementsByClassName("bank-one");
+            bank.current.classList.remove("active");
+        }
+
+        const changedTwo = (e) => {
+            let bank = document.getElementsByClassName("bank-two");
+            console.log(bank)
+            bank.current.classList.remove("active");
+        }
 
         return (
-            <div id="wrapper">
-                <div id="drum-machine" className="container">
+            <div id="wrapper" className="main">
+
+
+                <div id="drum-machine">
                     <div className="sampler">
                         <div className="strap one"></div>
                         <div className="strap two"></div>
@@ -47,21 +66,53 @@ export default class App extends React.Component {
                             <div className="strip-three"></div>
                             <div className="strip-four"></div>
                         </div>
-                        <div id="display" className="screen"><span className='screen-info'></span></div>
+                        <div className="screen">
+                            <span id="display" className='screen-info'></span></div>
                         <div id="slider"></div>
-                        <div className="bank-one active"></div>
-                        <div className="bank-two"></div>
+                        
                         <div id="drums-container" >
 
                         </div>
                         <div className="pad-container">
-                            {
-                                this.state.sounds.map(el => <div id={el.key} name={el.id} key={el.id} onClick={handleClick} className="pad drum-pad" >
-                                    {el.key}
-                                    <audio id={el.key} className="clip" src={el.url}></audio>
-                                </div>)
-                            }
-                        </div>
+                        
+                        <Tabs>
+                    <TabList>
+                        <Tab className=" bank-one">One</Tab>
+                        <Tab className=" bank-two">Two</Tab>
+                    </TabList>
+
+                    <TabPanels>
+                        <TabPanel>
+                        <div className="drum-pad">
+                        {
+                                this.state.sounds.map(el =>
+                                    <div id={el.key}
+                                        name={el.id}
+                                        key={el.id}
+                                        onClick={handleClick}
+                                        className="pad drum-pad">{el.key}
+
+                                        <audio id={el.key} className="clip" src={el.url}></audio>
+                                    </div>)
+                            }</div>
+                        </TabPanel>
+                        <TabPanel>
+                        <div className="drum-pad">
+                        {
+                                this.state.sounds.map(el =>
+                                    <div id={el.key}
+                                        name={el.id}
+                                        key={el.id}
+                                        onClick={handleClick}
+                                        className="pad drum-pad">{el.key}
+
+                                        <audio id={el.key} className="clip" src={el.url}></audio>
+                                    </div>)
+                            }</div>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+                </div>
                         <div className="bottom-plate"></div>
                     </div>
                 </div></div>
